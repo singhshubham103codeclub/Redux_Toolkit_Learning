11b39115054b8bbb784c7de08df4221fa6fc5d4c
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2026-07-27 09:59:21 +0530

    Add Redux store configuration and integrate with App component
    - Created a new store.js file to configure Redux store.
    - Updated main.jsx to wrap App component with Redux Provider.
    - Added @reduxjs/toolkit and react-redux dependencies in package.json and package-lock.json.
    - Simplified App component structure.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<provider store={store}>
+      <App />
+</provider>
   </StrictMode>,
 )
